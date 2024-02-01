aba3f86fef69b96b208080a17fe939941b1ba891
Sheng-En Huang <huang.sh@c1109a-s23.ufhpc>
2024-02-01 13:08:02 -0500

    debugging backup

---
+++ test5.py
@@ -0,0 +1,306 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import numpy as np
+from torch import nn
+import math
+from torch.optim import Adam
+
+
+
+def show_images(datset, num_samples=20, cols=4):
+    """ Plots some samples from the dataset """
+    plt.figure(figsize=(15,15))
+    for i, img in enumerate(data):
+        if i == num_samples:
+            break
+        plt.subplot(int(num_samples/cols) + 1, cols, i + 1)
+        plt.imshow(img[0])
+        plt.savefig("logs/show_images4.png") 
+
+data = torchvision.datasets.StanfordCars(root="./", download=True) # this can be solved by https://github.com/pytorch/vision/issues/7545
+data=torchvision.datasets.ImageFolder('source')
+show_images(data)
+
+def linear_beta_schedule(timesteps, start=0.0001, end=0.02):
+    return torch.linspace(start, end, timesteps)
+
+def get_index_from_list(vals, t, x_shape):
+    """
+    Returns a specific index t of a passed list of values vals
+    while considering the batch dimension.
+    """
+    batch_size = t.shape[0]
+    out = vals.gather(-1, t.cpu())
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+
+def forward_diffusion_sample(x_0, t, device="cpu"):
+    """
+    Takes an image and a timestep as input and
+    returns the noisy version of it
+    """
+    noise = torch.randn_like(x_0)
+    sqrt_alphas_cumprod_t = get_index_from_list(sqrt_alphas_cumprod, t, x_0.shape)
+    sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
+        sqrt_one_minus_alphas_cumprod, t, x_0.shape
+    )
+    # mean + variance
+    return sqrt_alphas_cumprod_t.to(device) * x_0.to(device) \
+    + sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device), noise.to(device)
+
+
+# Define beta schedule
+T = 5000
+betas = linear_beta_schedule(timesteps=T)
+
+# Pre-calculate different terms for closed form
+alphas = 1. - betas
+alphas_cumprod = torch.cumprod(alphas, axis=0)
+alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+
+
+
+
+IMG_SIZE = 128
+BATCH_SIZE = 5
+
+def load_transformed_dataset():
+    data_transforms = [
+        transforms.Resize((IMG_SIZE, IMG_SIZE)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(), # Scales data into [0,1]
+        transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1]
+    ]
+    data_transform = transforms.Compose(data_transforms)
+
+    train = torchvision.datasets.StanfordCars(root=".", download=True,
+                                         transform=data_transform)
+
+    test = torchvision.datasets.StanfordCars(root=".", download=True,
+                                         transform=data_transform, split='test')
+    train_data=torchvision.datasets.ImageFolder('source',transform=data_transform)
+    return train_data
+def show_tensor_image(image):
+    reverse_transforms = transforms.Compose([
+        transforms.Lambda(lambda t: (t + 1) / 2),
+        transforms.Lambda(lambda t: t.permute(1, 2, 0)), # CHW to HWC
+        transforms.Lambda(lambda t: t * 255.),
+        transforms.Lambda(lambda t: t.numpy().astype(np.uint8)),
+        transforms.ToPILImage(),
+    ])
+    # Take first image of batch
+    if len(image.shape) == 4:
+        image = image[0, :, :, :]
+    plt.imshow(reverse_transforms(image))
+
+data = load_transformed_dataset()
+dataloader = DataLoader(data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+
+
+
+# Simulate forward diffusion
+image = next(iter(dataloader))[0]
+
+plt.figure(figsize=(15,15))
+plt.axis('off')
+num_images = 10
+stepsize = int(T/num_images)
+
+for idx in range(0, T, stepsize):
+    t = torch.Tensor([idx]).type(torch.int64)
+    plt.subplot(1, num_images+1, int(idx/stepsize) + 1)
+    img, noise = forward_diffusion_sample(image, t)
+    show_tensor_image(img)
+
+
+
+
+
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch, time_emb_dim, up=False):   # 這邊是建立 operation
+        super().__init__()
+        self.time_mlp =  nn.Linear(time_emb_dim, out_ch)
+        if up:
+            self.conv1 = nn.Conv2d(2*in_ch, out_ch, 3, padding=1)
+            self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1)  # deconvolution (upsampling) 這邊往上變2倍
+        else:
+            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+            self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+        self.bnorm1 = nn.BatchNorm2d(out_ch)
+        self.bnorm2 = nn.BatchNorm2d(out_ch)
+        self.relu  = nn.ReLU()
+
+    def forward(self, x, t, ):   # 這邊是實際執行的部份。 這邊的double convolution 是Unet的精神  這裡也把 time step information 考慮進去了
+        # First Conv               這邊position embedding 就是加在每一個 residual block 裡面
+        h = self.bnorm1(self.relu(self.conv1(x)))
+        # Time embedding
+        time_emb = self.relu(self.time_mlp(t))
+        # Extend last 2 dimensions
+        time_emb = time_emb[(..., ) + (None, ) * 2]
+        # Add time channel     Add time embedding here 
+        h = h + time_emb
+        # Second Conv
+        h = self.bnorm2(self.relu(self.conv2(h)))
+        # Down or Upsample
+        return self.transform(h)
+
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        # TODO: Double check the ordering here
+        return embeddings
+
+
+class SimpleUnet(nn.Module):
+    """
+    A simplified variant of the Unet architecture.
+    """
+    def __init__(self):
+        super().__init__()
+        image_channels = 3
+        down_channels = (128, 256, 512, 1024, 2048)
+        up_channels = (2048, 1024, 512, 256, 128)
+        out_dim = 3
+        time_emb_dim = 32
+
+        # Time embedding
+        self.time_mlp = nn.Sequential(
+                SinusoidalPositionEmbeddings(time_emb_dim),
+                nn.Linear(time_emb_dim, time_emb_dim),
+                nn.ReLU()
+            )
+
+        # Initial projection
+        self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1) #這邊做了 downsample
+
+        # Downsample                                 #做完 downsample 之後立刻接了 由剛剛block 建立的double convolution
+        self.downs = nn.ModuleList([Block(down_channels[i], down_channels[i+1], \
+                                    time_emb_dim) \
+                    for i in range(len(down_channels)-1)])
+        # Upsample
+        self.ups = nn.ModuleList([Block(up_channels[i], up_channels[i+1], \
+                                        time_emb_dim, up=True) \
+                    for i in range(len(up_channels)-1)])
+
+        # Edit: Corrected a bug found by Jakub C (see YouTube comment)
+        self.output = nn.Conv2d(up_channels[-1], out_dim, 1)
+
+    def forward(self, x, timestep):
+        # Embedd time
+        t = self.time_mlp(timestep)
+        # Initial conv
+        x = self.conv0(x)
+        # Unet
+        residual_inputs = []
+        for down in self.downs:
+            x = down(x, t)
+            residual_inputs.append(x)         # 要將每一層與等等 upsample 對接
+        for up in self.ups:
+            residual_x = residual_inputs.pop()
+            # Add residual x as additional channels
+            x = torch.cat((x, residual_x), dim=1)   # 這邊就是對接 concat
+            x = up(x, t)
+        return self.output(x)
+
+model = SimpleUnet()
+print("Num params: ", sum(p.numel() for p in model.parameters()))
+print(model)
+
+def get_loss(model, x_0, t):
+    x_noisy, noise = forward_diffusion_sample(x_0, t, device)
+    noise_pred = model(x_noisy, t)    # 所以 u-net 就是想進辦法預測雜訊 （與輸入圖片等大小）
+    return F.l1_loss(noise, noise_pred)
+
+@torch.no_grad()
+def sample_timestep(x, t):
+    """
+    Calls the model to predict the noise in the image and returns
+    the denoised image.
+    Applies noise to this image, if we are not in the last step yet.
+    """
+    betas_t = get_index_from_list(betas, t, x.shape)
+    sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
+        sqrt_one_minus_alphas_cumprod, t, x.shape
+    )
+    sqrt_recip_alphas_t = get_index_from_list(sqrt_recip_alphas, t, x.shape)
+
+    # Call model (current image - noise prediction)
+    model_mean = sqrt_recip_alphas_t * (
+        x - betas_t * model(x, t) / sqrt_one_minus_alphas_cumprod_t
+    )
+    posterior_variance_t = get_index_from_list(posterior_variance, t, x.shape)
+
+    if t == 0:
+        # As pointed out by Luis Pereira (see YouTube comment)
+        # The t's are offset from the t's in the paper
+        return model_mean
+    else:
+        noise = torch.randn_like(x)
+        return model_mean + torch.sqrt(posterior_variance_t) * noise
+
+@torch.no_grad()
+def sample_plot_image(epochs, T, IMG_SIZE):
+    # Sample noise
+    img_size = IMG_SIZE
+    img = torch.randn((1, 3, img_size, img_size), device=device)
+    plt.figure(figsize=(15,15))
+    plt.axis('off')
+    num_images = 10
+    stepsize = int(T/num_images)
+
+    for i in range(0,T)[::-1]:
+        t = torch.full((1,), i, device=device, dtype=torch.long)
+        img = sample_timestep(img, t)
+        # Edit: This is to maintain the natural range of the distribution
+        img = torch.clamp(img, -1.0, 1.0)
+        if i % stepsize == 0:
+            show_tensor_image(img.detach().cpu())
+    plt.show()
+    plt.savefig("logs/sample_plot_image_"+"IMG_SIZE_"+str(IMG_SIZE)+"_epochs_"+str(epochs)+"_T_"+str(T)+".png") 
+
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+optimizer = Adam(model.parameters(), lr=0.0001)
+epochs = 10 # Try more!
+
+for epoch in range(epochs):
+    for step, batch in enumerate(dataloader):
+      optimizer.zero_grad()
+
+      t = torch.randint(0, T, (BATCH_SIZE,), device=device).long()
+      loss = get_loss(model, batch[0], t)
+      loss.backward()
+      optimizer.step()
+
+      if epoch % 5 == 0 and step == 0:
+        print(f"Epoch {epoch} | step {step:03d} Loss: {loss.item()} ")
+        sample_plot_image(epochs, T, IMG_SIZE)
+        
+        
+save_dir = 'models/model.pt'   #保存路径
+
+# 保存+读取整个模型
+torch.save(model, save_dir)
+# loaded_model = torch.load(save_dir)
+# loaded_model.cuda()
