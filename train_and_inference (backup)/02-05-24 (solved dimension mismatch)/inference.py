a21bc47e9cb97968b07f942860158ff317546819
Sheng-En Huang <huang.sh@c1003a-s11.ufhpc>
2024-02-06 13:22:47 -0500

    backup

---
+++ train_and_inference (backup)/02-05-24 (solved dimension mismatch)/inference.py
@@ -0,0 +1,133 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import numpy as np
+from torch import nn
+import math
+from torch.optim import Adam
+from utils import *
+from utils_model import *
+
+
+# Hyperparameters and parameter
+T = 1000
+IMG_SIZE = 128
+BATCH_SIZE = 5
+# Image Folder
+ImageF = 'source'
+# Load model
+load_dir = 'models/model.pt'   
+model = torch.load(load_dir)    
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)    
+
+# Define beta schedule
+betas = linear_beta_schedule(timesteps=T)
+# Pre-calculate different terms for closed form
+alphas = 1. - betas
+alphas_cumprod = torch.cumprod(alphas, axis=0)
+alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+
+def forward_diffusion_sample(x_0, t, device="cpu"):
+    """
+    Takes an image and a timestep as input and
+    returns the noisy version of it
+    """
+    noise = torch.randn_like(x_0)
+    sqrt_alphas_cumprod_t = get_index_from_list(sqrt_alphas_cumprod, t, x_0.shape)
+    sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
+        sqrt_one_minus_alphas_cumprod, t, x_0.shape
+    )
+    # mean + variance
+    return sqrt_alphas_cumprod_t.to(device) * x_0.to(device) \
+    + sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device), noise.to(device)
+
+def load_transformed_dataset():
+    data_transforms = [
+        transforms.Resize((IMG_SIZE, IMG_SIZE)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(), # Scales data into [0,1]
+        transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1]
+    ]
+    data_transform = transforms.Compose(data_transforms)
+    train_data=torchvision.datasets.ImageFolder(ImageF ,transform=data_transform)
+    return train_data
+
+def show_tensor_image(image):
+    reverse_transforms = transforms.Compose([
+        transforms.Lambda(lambda t: (t + 1) / 2),
+        transforms.Lambda(lambda t: t.permute(1, 2, 0)), # CHW to HWC
+        transforms.Lambda(lambda t: t * 255.),
+        transforms.Lambda(lambda t: t.numpy().astype(np.uint8)),
+        transforms.ToPILImage(),
+    ])
+    # Take first image of batch
+    if len(image.shape) == 4:
+        image = image[0, :, :, :]
+    plt.imshow(reverse_transforms(image))
+
+@torch.no_grad()
+def sample_timestep(x, t):
+    """
+    Calls the model to predict the noise in the image and returns
+    the denoised image.
+    Applies noise to this image, if we are not in the last step yet.
+    """
+    betas_t = get_index_from_list(betas, t, x.shape)
+    sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
+        sqrt_one_minus_alphas_cumprod, t, x.shape
+    )
+    sqrt_recip_alphas_t = get_index_from_list(sqrt_recip_alphas, t, x.shape)
+
+    # Call model (current image - noise prediction)
+    model_mean = sqrt_recip_alphas_t * (
+        x - betas_t * model(x, t) / sqrt_one_minus_alphas_cumprod_t
+    )
+    posterior_variance_t = get_index_from_list(posterior_variance, t, x.shape)
+
+    if t == 0:
+        # As pointed out by Luis Pereira (see YouTube comment)
+        # The t's are offset from the t's in the paper
+        return model_mean
+    else:
+        noise = torch.randn_like(x)
+        return model_mean + torch.sqrt(posterior_variance_t) * noise
+
+@torch.no_grad()
+def sample_plot_image():
+    # Sample noise
+    img_size = IMG_SIZE
+    img = torch.randn((1, 3, img_size, img_size), device=device)
+    plt.figure(figsize=(15,15))
+    plt.axis('off')
+    num_images = 10
+    stepsize = int(T/num_images)
+
+    for i in range(0,T)[::-1]:
+        t = torch.full((1,), i, device=device, dtype=torch.long)
+        img = sample_timestep(img, t)
+        # Edit: This is to maintain the natural range of the distribution
+        img = torch.clamp(img, -1.0, 1.0)
+    
+    show_tensor_image(img.detach().cpu())
+    plt.show()
+    plt.savefig("logs/sample_plot_image_inference_"+".png") 
+
+
+if __name__ == '__main__':
+    data=torchvision.datasets.ImageFolder(ImageF)
+    show_images(data)
+    data = load_transformed_dataset()
+    dataloader = DataLoader(data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+    sample_plot_image()
+
+        
+        
